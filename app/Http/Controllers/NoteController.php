e0b0bfda5cb410848f757060f856787d9a26dc56
Thirus <thirusofficial@gmail.com>
2022-02-21 23:37:29 +0530

    Add 'index' method

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -15,7 +15,7 @@ class NoteController extends Controller
      */
     public function index()
     {
-        $notes = Note::where('user_id', Auth::id())->latest('updated_at');
+        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->get();
         
     }
 
