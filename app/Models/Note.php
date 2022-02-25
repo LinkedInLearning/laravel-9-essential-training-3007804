65ee872598ac98f30653bfc37957187b78b37acf
Thirus <thirusofficial@gmail.com>
2022-02-25 23:17:44 +0530

    Define relationships

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -15,4 +15,8 @@ class Note extends Model
     {
         return 'uuid';
     }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Note;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function notes() {
+        return $this->hasMany(Note::class);
+    }
 }
