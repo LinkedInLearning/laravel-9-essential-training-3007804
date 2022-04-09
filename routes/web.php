9ff7739f125e5b8bb68d1082f616348db94654a2
Thirus <thirusofficial@gmail.com>
2022-04-09 23:40:24 +0530

    Work with Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
