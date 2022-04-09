05e559f5c5c531e7138e96b3d15bfdca195144a5
Thirus <thirusofficial@gmail.com>
2022-04-10 00:05:09 +0530

    Use Eloquent

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function index() {
         // 1. Using raw SQL queries
-        //$users = DB::select('select * from users');
-        //dd($users);
+        // $users = DB::select('select * from users');
+        // dd($users);
         
         // 2. Query Builder
-        $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-        dd($users);
+        // $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($users);
 
         // 3. Eloquent ORM
+        // $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        // dd($students);
+        // foreach($students as $student) {
+        //     echo $student->name. "<br>";
+        // }
+        $student = new Student();
+        $student->name = "New";
+        $student->email = "email@email.com";
+        $student->save();
+
     }
 }
