dca2aef56816ea25938b2a428f4b8c025c67c2d4
Thirus <thirusofficial@gmail.com>
2022-04-09 23:31:46 +0530

    Work with Controllers

--- resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts/base')
-
-@section('name','Shruti')
-
-@section('content')
-   <h1 class="text-4xl font-bold">About</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/home', function () {
-    return view('home', ['name' => "Shruti"]);
-});
-
-Route::get('/about', function () {
-    return view('about');
+Route::get('/', function () {
+    return view('welcome');
 });
