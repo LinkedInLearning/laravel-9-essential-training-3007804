c2754edb2d761077ee97f48c071acf0125910b82
Thirus <thirusofficial@gmail.com>
2022-04-09 23:06:24 +0530

    Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/home', function () {
     return view('welcome');
 });
