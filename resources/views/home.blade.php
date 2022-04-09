bdeb596c98bfbb8b0a84913ce21b4cf00936a23b
Thirus <thirusofficial@gmail.com>
2022-04-09 23:41:24 +0530

    Work with Controllers

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-base>
+   <x-slot name="name">Shruti</x-slot>
+   <x-slot name="content">
+      <h1 class="text-4xl font-bold">Home</h1>
+      <p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex recusandae impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati asperiores.</p>
+   </x-slot>
+</x-base>
