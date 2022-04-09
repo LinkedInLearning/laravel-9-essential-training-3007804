e2e1b3b9adc2d6e030b3af6a8c2e3d73497486ec
Thirus <thirusofficial@gmail.com>
2022-04-09 23:54:09 +0530

    Work with the database

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class WelcomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+        // 1. Using raw SQL queries
+        //$users = DB::select('select * from users');
+        //dd($users);
+        
+        // 2. Query Builder
+        $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        dd($users);
+
+        // 3. Eloquent ORM
     }
 }
